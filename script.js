d67c492cd6dbac1b08df0b4aaf33fba06eb03a76
jared_gd <jaredjodin@gmail.com>
2026-08-06 19:57:35 +0200

    Implement player management and game functionality
    This script manages a simple game where players can be added, and their scores can be modified. It includes functionality for starting a new game, saving game state, and loading previous game data.

--- script.js
+++ script.js
@@ -1,1 +1,178 @@
+let players = [];
 
+let gameStarted = false;
+
+
+// Boutons principaux
+
+const newGameButton = document.querySelector(".main-button");
+const home = document.getElementById("home");
+const game = document.getElementById("game");
+
+const playersContainer = document.getElementById("players-container");
+
+const addPlayerButton = document.getElementById("add-player");
+const startButton = document.getElementById("start-game");
+
+
+// Nouvelle partie
+
+newGameButton.addEventListener("click", () => {
+
+    home.classList.add("hidden");
+    game.classList.remove("hidden");
+
+});
+
+
+// Ajouter un joueur
+
+addPlayerButton.addEventListener("click", () => {
+
+    if(gameStarted){
+        return;
+    }
+
+
+    let name = prompt("Nom du joueur :");
+
+
+    if(name && name.trim() !== ""){
+
+        players.push({
+
+            name: name,
+            score: 0
+
+        });
+
+
+        saveGame();
+        displayPlayers();
+
+    }
+
+});
+
+
+
+// Démarrer la partie
+
+startButton.addEventListener("click", () => {
+
+    if(players.length < 1){
+
+        alert("Ajoute au moins un joueur !");
+        return;
+
+    }
+
+
+    gameStarted = true;
+
+    startButton.style.display = "none";
+
+    displayPlayers();
+
+});
+
+
+
+// Afficher les joueurs
+
+function displayPlayers(){
+
+    playersContainer.innerHTML = "";
+
+
+    players.forEach((player,index)=>{
+
+
+        let card = document.createElement("div");
+
+        card.className = "player-card";
+
+
+        card.innerHTML = `
+
+            <h3>${player.name}</h3>
+
+            <div class="score">
+                ${player.score}
+            </div>
+
+
+            <div class="score-buttons">
+
+                <button onclick="changeScore(${index},1)">
+                    +1
+                </button>
+
+
+                <button onclick="changeScore(${index},-1)">
+                    -1
+                </button>
+
+            </div>
+
+        `;
+
+
+        playersContainer.appendChild(card);
+
+
+    });
+
+}
+
+
+
+// Modifier un score
+
+function changeScore(index,value){
+
+
+    players[index].score += value;
+
+
+    saveGame();
+
+    displayPlayers();
+
+
+}
+
+
+
+// Sauvegarde automatique
+
+function saveGame(){
+
+    localStorage.setItem(
+        "BoardScoreSave",
+        JSON.stringify(players)
+    );
+
+}
+
+
+
+// Charger une ancienne partie
+
+function loadGame(){
+
+    let save = localStorage.getItem("BoardScoreSave");
+
+
+    if(save){
+
+        players = JSON.parse(save);
+
+        displayPlayers();
+
+    }
+
+}
+
+
+loadGame();
